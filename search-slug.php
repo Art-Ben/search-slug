e4a5585299f348c4a0d6074e3435c9d2307e254d
Art-Ben <artem.benevskiy@gmail.com>
2023-04-07 13:03:19 +0300

    FIX | Delete JS and Ajax hadler for menu | Added new approach to nav menu search

--- search-slug.php
+++ search-slug.php
@@ -55,43 +55,16 @@ add_filter('acf/fields/post_object/query', 'slug_search_acf', 10, 3);
 add_filter('acf/fields/page_link/query', 'slug_search_acf', 10, 3);
 add_filter('acf/fields/relationship/query', 'slug_search_acf', 10, 3);
 
-
-function search_slug_enqueue_scripts($hook_suffix) {
-    if ('nav-menus.php' === $hook_suffix) { // only load on nav-menus.php
-        wp_enqueue_script('search-by-slug', plugin_dir_url(__FILE__) . 'js/search-by-slug.js', ['jquery'], '1.0.0', true);
-        wp_localize_script('search-slug', 'SearchSlug', [
-            'ajaxurl' => admin_url('admin-ajax.php'),
-            'nonce' => wp_create_nonce('search-slug-nonce'),
-        ]);
-    }
+function search_slug_nav_menu( $q ) {
+	if(isset($_POST['action']) && $_POST['action']=="menu-quick-search" && isset($_POST['menu-settings-column-nonce'])){ // check if we are in the menu admin page and in action for quick search
+		$search_term = str_replace('slug:', '', $_POST['q']);
+		if( is_a($q->query_vars['walker'], 'Walker_Nav_Menu_Checklist') ){
+			$q->query_vars['name'] =  $search_term;
+			unset($q->query_vars['s']); // remove the search term, because without this, the search will return nothing
+		}
+	}
+	return $q;
 }
-add_action('admin_enqueue_scripts', 'search_slug_enqueue_scripts');
 
-function search_slug_ajax_handler_nav_menu() {
-    check_ajax_referer('search-slug-nonce', 'nonce');
-
-    if (isset($_POST['slug']) && isset($_POST['post_type'])) {
-        $slug = sanitize_text_field($_POST['slug']);
-        $post_type = sanitize_text_field($_POST['post_type']);
-
-        $posts = get_posts([
-            'post_type' => $post_type,
-            'post_name__in' => $slug,
-            'posts_per_page' => -1,
-        ]);
-
-        $found_items = [];
-
-        if (!empty($posts)) {
-            foreach ($posts as $post) {
-                $menu_item = wp_setup_nav_menu_item($post); // set up menu item object with all necessary properties
-                $menu_item->url = get_permalink($post->ID);
-                $found_items[] = $menu_item;
-            }
-        }
-
-        wp_send_json_success($found_items);
-    }
-}
-add_action('wp_ajax_search_slug_nav_menu', 'search_slug_ajax_handler_nav_menu');
+add_action( 'pre_get_posts', 'search_slug_nav_menu', 10, 2 );
 
